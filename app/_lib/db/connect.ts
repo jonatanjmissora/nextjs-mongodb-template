fb873274a5b295ed123f7a8c2d695195e76f5f8d
Jonatan <jonatanjmissora@gmail.com>
2025-03-23 00:22:10 -0300

    zod and mongodb. connect. env.local. meta. ping to mongo

---
+++ app/_lib/db/connect.ts
@@ -0,0 +1,39 @@
+import { MongoClient } from "mongodb"
+
+let client: MongoClient
+let clientPromise: Promise<MongoClient>
+const URI = process.env.MONGODB_URI!
+const DB = process.env.MONGODB_DB!
+const options = {}
+declare global {
+    var _mongoClientPromise: Promise<MongoClient>
+  }
+
+if (!URI) {
+  throw new Error("Please add your MongoDB URI to the .env file")
+}
+
+if (process.env.NODE_ENV === "development") {
+  // In development mode, use a global variable so that the MongoClient instance is not recreated.
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(URI, options)
+    global._mongoClientPromise = client.connect()
+  }
+  clientPromise = global._mongoClientPromise
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(URI, options)
+  clientPromise = client.connect()
+}
+
+async function getDatabase() {
+  const client = await clientPromise
+  return client.db(DB)
+}
+
+export async function getCollection(collectionName: string) {
+  const db = await getDatabase()
+  return db.collection(collectionName)
+}
+
+export default getDatabase
