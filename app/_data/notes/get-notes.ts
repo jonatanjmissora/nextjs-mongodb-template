6a9480630d1fc93e4aa7f4b92eb92967b93fa279
Jonatan <jonatanjmissora@gmail.com>
2025-03-30 22:43:53 -0300

    arreglado el unstable_cache

--- app/_data/notes/get-notes.ts
+++ app/_data/notes/get-notes.ts
@@ -1,19 +1,22 @@
 import { getNotesDB } from "@/app/_lib/db/notes"
 import authUser from "../auth/auth-user"
-import { NoteType } from "@/app/_lib/types/note-type"
 import { KindeUserType } from "@/app/_lib/types/user-type"
 import { unstable_cache } from "next/cache"
 
-export const getNotesAction = unstable_cache(async () => {
-    
-    const user = await authUser() as KindeUserType
-    const notes = await getNotesDB(user.id) as NoteType[]
-
-    return { user, notes}
+const getNotes = unstable_cache(async(userId: string) => {
+    console.log("NEW FETCH")
+    return await getNotesDB(userId)
 },
 ["notes"],
-{
-    tags: ["notes"],
-    revalidate: 3600,
-}
+    {
+      tags: ["notes"],
+      revalidate: 3600,
+    }
 )
+
+export const getNotesAction = async () => {
+    
+    const user = await authUser() as KindeUserType
+    const notes = await getNotes(user.id)
+    return {user, notes}
+}
