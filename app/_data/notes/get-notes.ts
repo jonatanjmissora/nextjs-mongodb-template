dc48d3eb0055a365128971fbbde9eb36bcfc2cd5
Jonatan <jonatanjmissora@gmail.com>
2025-03-30 21:49:50 -0300

    add unestable_cache

--- app/_data/notes/get-notes.ts
+++ app/_data/notes/get-notes.ts
@@ -2,11 +2,18 @@ import { getNotesDB } from "@/app/_lib/db/notes"
 import authUser from "../auth/auth-user"
 import { NoteType } from "@/app/_lib/types/note-type"
 import { KindeUserType } from "@/app/_lib/types/user-type"
+import { unstable_cache } from "next/cache"
 
-export const getNotesAction = async () => {
+export const getNotesAction = unstable_cache(async () => {
     
     const user = await authUser() as KindeUserType
     const notes = await getNotesDB(user.id) as NoteType[]
 
     return { user, notes}
+},
+["notes"],
+{
+    tags: ["notes"],
+    revalidate: 3600,
 }
+)
