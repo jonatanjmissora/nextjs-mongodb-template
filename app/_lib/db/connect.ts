4ecffadf0407514852c2b1b89dc6700bc9828b77
Jonatan Missora <jonatanjmissora@gmail.com>
2025-04-01 10:54:58 -0300

    connect mongodb fix to deploy

--- app/_lib/db/connect.ts
+++ app/_lib/db/connect.ts
@@ -1,23 +1,27 @@
 import { MongoClient } from "mongodb"
 
+let client: MongoClient
 let clientPromise: Promise<MongoClient>
+
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient> | undefined
+}
 const URI = process.env.MONGODB_URI!
 const DB = process.env.MONGODB_DB!
 const options = {}
-declare global {
-  // eslint-disable-next-line no-var
-  var _mongoClientPromise: Promise<MongoClient>
-}
-const client: MongoClient = new MongoClient(URI, options)
+client = new MongoClient(URI, options)
 
 if (!URI) {
   throw new Error("Please add your MongoDB URI to the .env file")
 }
 
 try {
+
   if (process.env.NODE_ENV === "development") {
     // In development mode, use a global variable so that the MongoClient instance is not recreated.
     if (!global._mongoClientPromise) {
+      client = new MongoClient(URI, options)
       global._mongoClientPromise = client.connect()
     }
     clientPromise = global._mongoClientPromise
@@ -25,6 +29,7 @@ try {
     // In production mode, it's best to not use a global variable.
     clientPromise = client.connect()
   }
+
 } catch (error) {
   if (error instanceof Error)
     console.log("ERROR", error.stack);
@@ -33,7 +38,6 @@ finally {
   await client.close();
   console.log("CLOSE")
 }
-
 
 async function getDatabase() {
   const client = await clientPromise
